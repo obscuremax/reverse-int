21868b0d55ae73d728d2e8df8601fe29858fa14c
obscuremax <maxxam@yandex.ru>
2020-02-21 01:20:22 +0300

    resolve reverse-int

--- src/index.js
+++ src/index.js
@@ -1,3 +1,9 @@
 module.exports = function reverse (n) {
-  
+  let a = String(n).split("")
+    if(a[0] === "-") {
+        a.shift()
+    } else if (a[a.length-1] === "0") {
+        a.pop()
+    }
+    return Number(a.reverse().join(""))
 }
